9d7f674da55c544929e9e0a5ca809c04ad39e69c
ShixiongQi <qishixiong19931009@gmail.com>
2022-07-01 23:26:05 -0500

    Upgrade Knative

--- 400-kn-install.sh
+++ 400-kn-install.sh
@@ -1,6 +1,20 @@
-VERSION=0.7.1 # choose the latest version
-OS=Linux     # or Darwin
-ARCH=x86_64  # or arm64, i386, s390x
-curl -L https://github.com/google/ko/releases/download/v${VERSION}/ko_${VERSION}_${OS}_${ARCH}.tar.gz | tar xzf - ko
-sudo chmod +x ./ko
-sudo install ko /usr/bin
+Executable File  35 lines (27 sloc)  1.64 KB
+
+#!/bin/bash
+mount_path=$MYMOUNT
+
+if [[ $mount_path == "" ]]
+then
+	echo MYMOUNT env var not defined
+	exit 1
+fi
+
+echo 'Install Knative Serving'
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.5.0/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.5.0/serving-core.yaml
+
+echo 'Install Istio'
+kubectl apply -l knative.dev/crd-install=true -f https://github.com/knative/net-istio/releases/download/knative-v1.5.0/istio.yaml
+kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.5.0/istio.yaml
+kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.5.0/net-istio.yaml
+kubectl --namespace istio-system get service istio-ingressgateway
