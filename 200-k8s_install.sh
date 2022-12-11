ba707fd5903d50aba8609f08081c8a2f1a4f5376
gauravratnakar256 <93859207+gauravratnakar256@users.noreply.github.com>
2022-12-10 23:35:55 -0800

    Update 200-k8s_install.sh
    Updated kubernetes version

--- 200-k8s_install.sh
+++ 200-k8s_install.sh
@@ -35,7 +35,7 @@ function install_k8s_tools {
 	echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 	sudo apt-get update
-	sudo apt-get install -y kubelet kubeadm kubectl
+	sudo apt-get install -y kubelet=1.24.0-00 kubeadm=1.24.0-00 kubectl=1.24.0-00
 	sudo apt-mark hold kubelet=1.24.0-00 kubeadm=1.24.0-00 kubectl=1.24.0-00
 
 	sudo systemctl daemon-reload
